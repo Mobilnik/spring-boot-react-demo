794a7dbb4d20f708b63ad059bcb0ef9d7e30fdb0
ulitin.i <ulitin.i@milandr.ru>
2019-04-29 16:29:58 +0300

    Minor changes.

--- src/main/java/sb/milandr/demo/config/WebSecurityConfig.java
+++ src/main/java/sb/milandr/demo/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package sb.milandr.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-@Profile("!https")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -23,14 +21,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(final HttpSecurity http)
-            throws Exception {
+    protected void configure(final HttpSecurity http) throws Exception {
+        //https://ru.wikipedia.org/wiki/Межсайтовая_подделка_запроса
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/index*", "/static/**", "/*.js", "/*.json", "/*.ico")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/index.html")
+                .formLogin()
+                .loginPage("/index.html")
                 .loginProcessingUrl("/perform_login")
                 .defaultSuccessUrl("/homepage.html", true)
                 .failureUrl("/index.html?error=true");
